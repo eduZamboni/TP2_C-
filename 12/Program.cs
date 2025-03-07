19d47c26397ef0f38198710588b660058c115075
eduZamboni <128424472+eduZamboni@users.noreply.github.com>
2025-03-07 00:02:43 -0300

    Ultimo Commit

---
+++ 12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        int numeroSecreto = rnd.Next(1, 101);
+        int palpite = 0;
+
+        Console.WriteLine("Adivinhe o número (1 a 100):");
+
+        while (palpite != numeroSecreto)
+        {
+            Console.Write("Seu palpite: ");
+            palpite = int.Parse(Console.ReadLine());
+
+            if (palpite < numeroSecreto)
+                Console.WriteLine("O número é maior!");
+            else if (palpite > numeroSecreto)
+                Console.WriteLine("O número é menor!");
+            else
+                Console.WriteLine("Parabéns, você acertou!");
+        }
+    }
+}
